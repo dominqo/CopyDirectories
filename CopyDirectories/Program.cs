3ab3b72a60ca12e7c40a0200bd00852962410f79
Dominik Haczek <dominik.haczek@outlook.com>
2018-07-09 00:28:00 +0200

    0.001

---
+++ CopyDirectories/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace CopyDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0; i<ReadFile.countPC(); i++)
+            {
+                Console.WriteLine("Twoje PC numer {0} to jest numer {1}", i, ReadFile.loadPC(i));
+            }
+            DirectoryCopy(@"D:\Projekty\C#\PartyInvites", @"D:\Projekty\", true);
+            Console.ReadKey();
+        }
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
+        }
+    }
+}
---
+++ CopyDirectories/ReadFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace CopyDirectories
+{
+    static class ReadFile
+    {
+        //public static StreamReader file = new StreamReader("PC.txt");
+        public static int loadPC(int num)
+        {
+            int[] tablica = new int[countPC()];
+            StreamReader file = null;
+            try
+            {
+                file = new StreamReader("PC.txt");
+                int i = 0;
+                while (file.Peek() >= 0)
+                {
+                    tablica[i] = Int32.Parse(file.ReadLine());
+                    i++;
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found.");
+            }
+            finally
+            {
+                if (file != null)
+                    file.Dispose();
+            }
+            return tablica[num];
+        }
+        public static int countPC()
+        {
+            StreamReader file = new StreamReader("PC.txt");
+            int i = 0;
+            while (file.ReadLine() != null) { i++; }
+            return i;
+        }
+    }
+}
