a147d71d90abed8ecc3805eaa17e1cfeaa59c35b
Dominik Haczek <dominik.haczek@outlook.com>
2018-07-14 07:54:15 +0200

    przed queue

---
+++ CopyDirectories/Szukanie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace CopyDirectories
+{
+    static class Szukanie
+    {
+        public static string[] loadPC()
+        {
+            
+            StreamReader file = null;
+            try
+            {
+                file = new StreamReader("PC.txt");
+                int a = 0;
+                while (file.ReadLine() != null) { a++; }
+                string[] tablica = new string[a];
+                file.DiscardBufferedData();
+                file.BaseStream.Seek(0, System.IO.SeekOrigin.Begin);
+                for (int i = 0; i < a; i++)
+                {
+                    tablica[i] = file.ReadLine();
+                }
+                return tablica;
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found.");
+                string[] strings = new string[] { "No file" };
+                return strings;
+            }
+            finally
+            {
+                if (file != null)
+                    file.Dispose();
+            }
+        }
+    }
+}
